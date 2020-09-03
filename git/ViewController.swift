06bac88461a600e44ff91549635606a4e223119f
Yu <t102518034@gmail.com>
2020-09-03 16:13:19 +0800

    test

--- git/ViewController.swift
+++ git/ViewController.swift
@@ -9,12 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let message = "Hello Git!"
 
     override func viewDidLoad() {
+        print(message)
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
 
 }
 
